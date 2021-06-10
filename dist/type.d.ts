488f9ee2f2c73db60bba4ba76d3dc82db33c1f2d
李尔王 <46747508+happy-func@users.noreply.github.com>
2021-06-10 21:10:13 +0800

    Delete dist directory

--- dist/type.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type sudokuList = number[][];
-export interface genOptions {
-    mask?: boolean;
-    gzip?: boolean;
-}
--- dist/verify.d.ts
+++
@@ -1,3 +0,0 @@
-import { sudokuList } from "./type";
-declare const verify: (list: sudokuList) => boolean;
-export default verify;
