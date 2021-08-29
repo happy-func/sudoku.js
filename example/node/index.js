f5833e1183cc5d6edb192ff2b0db87f9d6ec0e06
happy-func <stevenhanth@gmail.com>
2021-08-29 11:10:34 +0800

    remove useless tip

--- example/node/index.js
+++ example/node/index.js
@@ -4,7 +4,6 @@ const sudoku = require("js-sudoku");
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   const list = sudoku.gen({ mask: false, gzip: true, level: sudoku.Level.HIGH });
-  // 发送响应数据 "Hello World"
   response.end(list);
 }).listen(8888);
 
